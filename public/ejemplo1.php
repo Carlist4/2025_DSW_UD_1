d60cb17c48df974e59077ea9f176d58940bdcbdd
Carlos Campos <camposcastillacarlos@gmail.com>
2025-09-18 17:40:45 +0000

    Ejemplo

---
+++ public/ejemplo1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Ejemplo 1</h1>
+    <?php
+        echo "<h2>Hola mundo\n<h2>";
+    ?>
+    <h3>
+        <?php
+            echo "Otra linea";
+        ?>
+    </h3>
+    <?php
+        $name = "Carlos";
+        echo "Mi nombre es $name";
+    ?>
+</body>
+</html>
